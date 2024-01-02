dae8041c4ee3ccd08d6dbfa6bc94a484e358cc05
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-02 19:40:48 +0900

    [level 1] Title: 음양 더하기, Time: 0.08 ms, Memory: 58.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.kt
@@ -0,0 +1,4 @@
+class Solution {
+    fun solution(absolutes: IntArray, signs: BooleanArray) =
+        absolutes.foldIndexed(0) { index, sum, value -> if (signs[index]) sum + value else sum + (value * -1) }
+}
