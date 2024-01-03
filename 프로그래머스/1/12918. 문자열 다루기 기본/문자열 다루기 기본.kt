61f0643d2d5e0413ffc08dd0dc2a77730f9dd230
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-03 14:32:38 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(s: String) = (s.length == 4 || s.length == 6) && s.all { it in '0'..'9' }
+    fun solution(s: String) = (s.length == 4 || s.length == 6) && s.all { it.isDigit() }
 }
