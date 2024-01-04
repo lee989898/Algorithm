097e8d2611001ab8fa07e9514174203bda545b1d
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-04 17:34:12 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 22.85 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.kt
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.kt
@@ -1,16 +1,15 @@
 class Solution {
     fun solution(s: String): IntArray {
         var copyS = s
-        var countZero = 0
+        var removedZero = 0
         var countConversion = 0
 
         while (copyS.length != 1) {
-            val removedZero = copyS.length - copyS.replace("0", "").count()
-            copyS = Integer.toBinaryString(copyS.length - removedZero).toString()
-            countZero += removedZero
-            countConversion += 1
+            removedZero += copyS.replace("1", "").count()
+            copyS = Integer.toBinaryString(copyS.replace("0", "").count())
+            countConversion++
         }
 
-        return intArrayOf(countConversion, countZero)
+        return intArrayOf(countConversion, removedZero)
     }
 }
