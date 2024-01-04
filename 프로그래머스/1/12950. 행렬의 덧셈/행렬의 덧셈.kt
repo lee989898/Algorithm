9f8faa11c36dc6a4ad2cc3b90d2d8cad4cb5d750
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-04 17:18:49 +0900

    [level 1] Title: 행렬의 덧셈, Time: 5.44 ms, Memory: 94.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.kt
@@ -0,0 +1,4 @@
+class Solution {
+    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> =
+        Array(arr1.size) { i -> IntArray(arr1[i].size) { j -> arr1[i][j] + arr2[i][j] } }
+}
