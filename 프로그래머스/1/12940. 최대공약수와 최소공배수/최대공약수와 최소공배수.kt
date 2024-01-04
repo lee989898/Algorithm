445e1bb35ef690cb00fc702e356a6a105d960a80
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-04 15:11:25 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 13.75 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun solution(n: Int, m: Int) = gcd(n, m).let { listOf(it, n * m / it) }
 
-    private fun gcd(num1: Int, num2: Int): Int {
+    private tailrec fun gcd(num1: Int, num2: Int): Int {
         return if (num1 % num2 == 0) {
             num2
         } else {
