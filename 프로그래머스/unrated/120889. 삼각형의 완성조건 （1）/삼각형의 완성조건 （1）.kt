96b101c34ec7d3a3b92d6436b2652947cee7beec
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-07 17:28:34 +0900

    [level unrated] Title: 삼각형의 완성조건 (1), Time: 18.15 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.kt
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(sides: IntArray) = sides.sorted().let { if (it[0] + it[1] > it[2]) 1 else 2 }
+    fun solution(sides: IntArray) = sides.sorted().let { (a, b, c) -> if (a + b > c) 1 else 2 }
 }
