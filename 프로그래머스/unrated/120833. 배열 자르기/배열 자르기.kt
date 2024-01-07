bacf932493c468549d8895f01b8b7426571eabcc
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-07 15:44:05 +0900

    [level unrated] Title: 배열 자르기, Time: 21.47 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.kt
+++ 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(numbers: IntArray, num1: Int, num2: Int) = numbers.toList().slice(num1..num2)
+    fun solution(numbers: IntArray, num1: Int, num2: Int) = numbers.sliceArray(num1..num2)
 }
