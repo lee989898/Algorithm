beb3fca1f9af3b0940ae170f86b76ba6ad7de68c
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-08 20:48:18 +0900

    [level unrated] Title: 마지막 두 원소, Time: 13.99 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.kt
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.kt
@@ -1,13 +1,7 @@
 class Solution {
-    fun solution(num_list: IntArray): IntArray {
-        var answer: IntArray = num_list
-
-        answer += if (num_list[num_list.lastIndex] > num_list[num_list.lastIndex - 1]) {
-            num_list[num_list.lastIndex] - num_list[num_list.lastIndex - 1]
-        } else {
-            num_list[num_list.lastIndex] * 2
-        }
-        
-        return answer
+    fun solution(num_list: IntArray) = num_list + if (num_list[num_list.lastIndex] > num_list[num_list.lastIndex - 1]) {
+        num_list[num_list.lastIndex] - num_list[num_list.lastIndex - 1]
+    } else {
+        num_list[num_list.lastIndex] * 2
     }
 }
