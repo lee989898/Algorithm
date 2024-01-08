e3776fa36801bacfb79106ea905bf773aa3e50de
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-09 00:03:06 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 5.87 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.kt
@@ -0,0 +1,7 @@
+import kotlin.math.max
+
+class Solution {
+    fun solution(num_list: IntArray) = max(num_list.filterIndexed { index, i -> index % 2 == 0 }.sum(),
+        num_list.filterIndexed { index, i -> index % 2 != 0 }.sum()
+    )
+}
