288589f08a6e4b5ec269998ea9f4481e6ccaf469
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-08 20:25:03 +0900

    [level unrated] Title: n 번째 원소부터, Time: 13.54 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.kt
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(num_list: IntArray, n: Int) = num_list.slice(n - 1..num_list.lastIndex).toIntArray()
+    fun solution(num_list: IntArray, n: Int) = num_list.copyOfRange(n - 1, num_list.size)
 }
