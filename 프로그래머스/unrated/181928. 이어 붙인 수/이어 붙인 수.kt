f26457ff2e3e415c245eb1853abaab3222e4e446
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-08 20:17:40 +0900

    [level unrated] Title: 이어 붙인 수, Time: 12.07 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.kt
@@ -0,0 +1,5 @@
+class Solution {
+    fun solution(num_list: IntArray) =
+        num_list.filter { it % 2 == 0 }.joinToString("").toInt() + num_list.filter { it % 2 != 0 }.joinToString("")
+            .toInt()
+}
