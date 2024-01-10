6ab72d9925b253de5f883893ce53917bbc63c906
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-10 20:26:41 +0900

    [level unrated] Title: 특별한 이차원 배열 2, Time: 0.01 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(arr: Array<IntArray>): Int {
+        for (i in arr.indices) {
+            for(j in arr.indices) {
+                if (arr[i][j] != arr[j][i]) {
+                    return 0
+                }
+            }
+        }
+
+        return 1
+    }
+}
