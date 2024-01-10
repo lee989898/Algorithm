d1657182a04d0519ef0f909b8b94eeef9092408d
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-10 17:36:37 +0900

    [level unrated] Title: 공백으로 구분하기 2, Time: 11.98 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.kt
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.kt
@@ -1,24 +1,3 @@
 class Solution {
-    fun solution(my_string: String): Array<String> {
-        var answer: Array<String> = arrayOf<String>()
-
-        var temp = ""
-
-        (my_string.indices).forEach {
-            if (my_string[it] == ' ') {
-                if (temp.isNotEmpty()) {
-                    answer += temp
-                }
-                temp = ""
-            } else {
-                temp += my_string[it]
-            }
-        }
-
-        if (temp.isNotEmpty()) {
-            answer += temp
-        }
-
-        return answer
-    }
+    fun solution(my_string: String) = my_string.split(" ").filterNot { it.isEmpty()}
 }
