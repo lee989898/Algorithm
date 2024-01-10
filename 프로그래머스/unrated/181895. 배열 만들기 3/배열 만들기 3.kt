dc19d75223d14d942879cd2dc54ce4fd62376d6d
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-10 22:43:15 +0900

    [level unrated] Title: 배열 만들기 3, Time: 24.87 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.kt
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.kt
@@ -1,4 +1,3 @@
 class Solution {
-    fun solution(arr: IntArray, intervals: Array<IntArray>) =
-        arr.slice(intervals[0][0]..intervals[0][1]) + arr.slice(intervals[1][0]..intervals[1][1])
+    fun solution(arr: IntArray, intervals: Array<IntArray>) = intervals.flatMap { arr.slice(it[0]..it[1]) }
 }
