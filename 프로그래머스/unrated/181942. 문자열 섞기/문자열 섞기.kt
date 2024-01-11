6f3d7261b404a33f70bbbc4e6fff060a6e1a9760
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-11 11:41:41 +0900

    [level unrated] Title: 문자열 섞기, Time: 28.74 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.kt
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(str1: String, str2: String) = str1.indices.joinToString("") { "${str1[it]}${str2[it]}" }
+    fun solution(str1: String, str2: String) = str1.zip(str2).joinToString("") { (a, b) -> "$a$b" }
 }
