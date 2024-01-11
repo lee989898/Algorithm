b688c2e34f82186109293c41ef07a7f337c7dca6
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-11 13:48:35 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.38 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.kt
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.kt
@@ -1,9 +1,7 @@
 class Solution {
     fun solution(arr: IntArray, queries: Array<IntArray>): IntArray {
-        queries.forEach {
-            val temp = arr[it[0]]
-            arr[it[0]] = arr[it[1]]
-            arr[it[1]] = temp
+        queries.forEach { (first, second) ->
+            arr[first] = arr[second].also { arr[second] = arr[first] }
         }
         return arr
     }
