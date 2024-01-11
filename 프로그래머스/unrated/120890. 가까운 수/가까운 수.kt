a4e6a0a23e7486da989efeb275c234f411ada95f
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-11 15:49:01 +0900

    [level unrated] Title: 가까운 수, Time: 33.23 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120890. 가까운 수/가까운 수.kt
+++ 프로그래머스/unrated/120890. 가까운 수/가까운 수.kt
@@ -1,22 +1,10 @@
 import kotlin.math.abs
 
 class Solution {
-    fun solution(array: IntArray, n: Int): Int {
-        var answer = Int.MAX_VALUE
-        var value = 0
-
-        array.forEach {
-
-            if (answer > abs(it - n)) {
-                answer = abs(it - n)
-                value = it
-            } else if (abs(it - n) == answer) {
-                if (value > it) {
-                    value = it
-                }
-            }
-        }
-
-        return value
-    }
+    fun solution(array: IntArray, n: Int) = array.sortedWith(
+        compareBy(
+            { abs(it - n) },
+            { it }
+        )
+    ).first()
 }
