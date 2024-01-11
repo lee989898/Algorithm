c868b63590ff9ada73d8e535698ec2422bc09cd2
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-11 11:58:48 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.02 ms, Memory: 62.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun solution(date1: IntArray, date2: IntArray) = if (date1[0] == date2[0]) {
+        if (date1[1] == date2[1]) {
+            if (date1[2] < date2[2]) {
+                1
+            } else {
+                0
+            }
+        } else if (date1[1] < date2[1]) {
+            1
+        } else {
+            0
+        }
+    } else if (date1[0] < date2[0]) {
+        1
+    } else {
+        0
+    }
+}
