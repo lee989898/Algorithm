1b23d41e5500940c2b3c2dcd6b7ce39321035d25
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-12 19:50:33 +0900

    [level Lv.0] Title: 조건에 맞게 수열 변환하기 2, Time: 24.45 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.kt
+++ 프로그래머스/Lv.0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.kt
@@ -4,16 +4,16 @@ class Solution {
         var prev = arr.toList()
 
         while (true) {
-            answer++
-
             val cur =
                 prev.map { if (it >= 50 && it % 2 == 0) it / 2 else if (it < 50 && it % 2 != 0) (it * 2) + 1 else it }
 
             if (cur == prev) break
 
             prev = cur
+            
+            answer++
         }
 
-        return answer - 1
+        return answer
     }
 }
