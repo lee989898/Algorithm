441eb448d3ddc6e18a8446346b90fc51b5d40c19
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-12 19:30:54 +0900

    [level Lv.0] Title: 이진수 더하기, Time: 0.88 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120885. 이진수 더하기/이진수 더하기.kt
+++ 프로그래머스/Lv.0/120885. 이진수 더하기/이진수 더하기.kt
@@ -1,3 +1,3 @@
 class Solution {
-    fun solution(bin1: String, bin2: String) = Integer.toBinaryString(bin1.toInt(2) + bin2.toInt(2))
+    fun solution(bin1: String, bin2: String) = (bin1.toInt(2) + bin2.toInt(2)).toString(2)
 }
