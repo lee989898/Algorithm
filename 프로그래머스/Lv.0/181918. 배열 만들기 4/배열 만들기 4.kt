603265c743874855d37a17901b459c262e268bc7
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-12 20:50:41 +0900

    [level Lv.0] Title: 배열 만들기 4, Time: 35.23 ms, Memory: 68.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181918. 배열 만들기 4/배열 만들기 4.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun solution(arr: IntArray): IntArray {
+        var stk: IntArray = intArrayOf()
+        var i = 0
+
+        while (i < arr.size) {
+            if (stk.isEmpty()) {
+                stk += arr[i]
+                i++
+            } else if (stk.last() < arr[i]) {
+                stk += arr[i]
+                i++
+            } else {
+                stk = stk.dropLast(1).toIntArray()
+            }
+        }
+
+        return stk
+    }
+}
