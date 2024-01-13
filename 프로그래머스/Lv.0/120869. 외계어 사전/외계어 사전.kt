9ec063a543cda80b98d431b83b5e71e9a3ab437b
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-13 17:52:11 +0900

    [level Lv.0] Title: 외계어 사전, Time: 32.38 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120869. 외계어 사전/외계어 사전.kt
+++ 프로그래머스/Lv.0/120869. 외계어 사전/외계어 사전.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun solution(spell: Array<String>, dic: Array<String>): Int {
-        val copySpell = spell.toSet()
-        return if (dic.count { d-> d.map { it.toString() }.toSet() == copySpell } == 0) 2 else 1
+        val copySpell = spell.joinToString("").toSet()
+        return if (dic.count { it.toSet() == copySpell } == 0) 2 else 1
     }
 }
