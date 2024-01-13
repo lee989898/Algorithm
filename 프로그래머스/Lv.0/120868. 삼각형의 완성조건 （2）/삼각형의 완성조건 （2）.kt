2cd1dc90a334dbd5ad103f02d9488674a5eeced8
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-13 17:21:47 +0900

    [level Lv.0] Title: 삼각형의 완성조건 (2), Time: 21.45 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.kt
+++ 프로그래머스/Lv.0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.kt
@@ -1,22 +1,5 @@
+import kotlin.math.abs
+
 class Solution {
-    fun solution(sides: IntArray): Int {
-        var answer = 0
-
-        val sidesMax = sides.maxOf { it }
-        val sidesMin = sides.minOf { it }
-        var other = sidesMax + 1
-        var cur = sidesMax
-
-        while (cur + sidesMin > sidesMax) {
-            answer++
-            cur--
-        }
-
-        while (sidesMin + sidesMax > other) {
-            answer++
-            other++
-        }
-
-        return answer
-    }
+    fun solution(sides: IntArray) = sides.sum() - abs(sides[0] - sides[1]) - 1
 }
