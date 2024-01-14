b85c3ee44ed8e62a6b81abbca21864ac2f95df98
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-14 20:46:57 +0900

    [level Lv.0] Title: 저주의 숫자 3, Time: 6.30 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120871. 저주의 숫자 3/저주의 숫자 3.kt
+++ 프로그래머스/Lv.0/120871. 저주의 숫자 3/저주의 숫자 3.kt
@@ -1,19 +1,15 @@
 class Solution {
     fun solution(n: Int): Int {
-        var answer = 1
-        val arr = ArrayList<Int>()
+        var answer = 0
 
-        if (n == 1) return 1
-        
-        while (arr.size != 100) {
-            if (answer % 3 != 0 && !answer.toString().contains("3")) {
-                arr.add(answer)
-                answer++
-            } else {
+        repeat(n) {
+            answer++
+
+            while (answer % 3 == 0 || answer.toString().contains("3")) {
                 answer++
             }
         }
-        
-        return arr[n - 1]
+
+        return answer
     }
 }
