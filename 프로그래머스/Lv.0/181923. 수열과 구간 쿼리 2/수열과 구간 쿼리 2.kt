6b9ff1d7c4ff9014cf958dc0739afbaa181cf7e7
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-14 17:03:07 +0900

    [level Lv.0] Title: 수열과 구간 쿼리 2, Time: 45.15 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.kt
@@ -0,0 +1,5 @@
+class Solution {
+    fun solution(arr: IntArray, queries: Array<IntArray>) = queries.map { (s, e, k) ->
+        arr.sliceArray(s..e).sorted().find { it > k } ?: -1
+    }.toIntArray()
+}
