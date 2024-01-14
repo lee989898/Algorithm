e558cf39c0698a27677cc5990b7a8ea161346226
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-14 22:05:31 +0900

    [level Lv.0] Title: 문자열 밀기, Time: 8.46 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120921. 문자열 밀기/문자열 밀기.kt
+++ 프로그래머스/Lv.0/120921. 문자열 밀기/문자열 밀기.kt
@@ -1,14 +1,3 @@
 class Solution {
-    fun solution(A: String, B: String): Int {
-        var answer = 0
-        var copyA = A
-
-        repeat(A.length) {
-            if (copyA == B) return answer
-            answer++
-            copyA = copyA.takeLast(1) + copyA.dropLast(1)
-        }
-
-        return -1
-    }
-}
+    fun solution(A: String, B: String) = "$B$B".indexOf(A)
+ }
