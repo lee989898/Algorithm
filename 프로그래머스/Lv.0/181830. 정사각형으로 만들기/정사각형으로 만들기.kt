18adb2c2635801daa233e784d4f22ea201d78109
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-14 16:55:13 +0900

    [level Lv.0] Title: 정사각형으로 만들기, Time: 10.43 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181830. 정사각형으로 만들기/정사각형으로 만들기.kt
+++ 프로그래머스/Lv.0/181830. 정사각형으로 만들기/정사각형으로 만들기.kt
@@ -1,22 +1,13 @@
+import kotlin.math.max
+
 class Solution {
     fun solution(arr: Array<IntArray>): Array<IntArray> {
-        var answer: Array<IntArray> = arr
+        val max = max(arr.size, arr[0].size)
+        val array = Array(max) { IntArray(max) }
 
-        val column = arr.size
-        val row = arr[0].size
-
-        if (column > row) {
-            answer.forEachIndexed { index, _ ->
-                repeat(column - row) {
-                    answer[index] = answer[index] + 0
-                }
-            }
-        } else if (row > column) {
-            repeat(row - column) {
-                answer += IntArray(row)
-            }
+        for (i in arr.indices) {
+            arr[i].copyInto(array[i])
         }
-
-        return answer
+        return array
     }
 }
