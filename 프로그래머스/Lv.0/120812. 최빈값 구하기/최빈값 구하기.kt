bbfdc0085a41ac94e96bd2c7c358fd18fbda2d8b
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-14 20:05:06 +0900

    [level Lv.0] Title: 최빈값 구하기, Time: 0.24 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120812. 최빈값 구하기/최빈값 구하기.kt
+++ 프로그래머스/Lv.0/120812. 최빈값 구하기/최빈값 구하기.kt
@@ -1,11 +1,11 @@
 class Solution {
     fun solution(array: IntArray): Int {
-        val group = array.groupBy { it }
+        val group = array.groupBy { it }.entries
         val max = group.maxOf { it.value.size }
-        val filter = group.filter { it.value.size == max }.toList()
-        
+        val filter = group.filter { it.value.size == max }
+
         return if (filter.size == 1) {
-            filter[0].first
+            filter[0].key
         } else {
             -1
         }
