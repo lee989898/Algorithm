5bf5367f11dcc8a1333d6cb4eeba18c6c4da6e7b
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-15 22:37:53 +0900

    [level Lv.0] Title: 평행, Time: 0.03 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120875. 평행/평행.kt
+++ 프로그래머스/Lv.0/120875. 평행/평행.kt
@@ -1,13 +1,13 @@
 import kotlin.math.abs
 
 class Solution {
-    fun solution(dots: Array<IntArray>) = if (abs(dots[0][1] - dots[1][1]).toDouble() / abs(dots[0][0] - dots[1][0])
-        == abs(dots[2][1] - dots[3][1]).toDouble() / abs(dots[2][0] - dots[3][0])
-        ||
-        (abs(dots[0][1] - dots[2][1]).toDouble() / abs(dots[0][0] - dots[2][0])
-                == abs(dots[1][1] - dots[3][1]).toDouble() / abs(dots[1][0] - dots[3][0]))
-        ||
-        (abs(dots[0][1] - dots[3][1]).toDouble() / abs(dots[0][0] - dots[3][0])
-                == abs(dots[1][1] - dots[2][1]).toDouble() / abs(dots[1][0] - dots[2][0]))
-    ) 1 else 0
+    fun solution(dots: Array<IntArray>): Int {
+        val (x1, x2, x3, x4) = intArrayOf(dots[0][0], dots[1][0], dots[2][0], dots[3][0])
+        val (y1, y2, y3, y4) = intArrayOf(dots[0][1], dots[1][1], dots[2][1], dots[3][1])
+
+        return if (abs((x1 - x2) * (y3 - y4)) == abs((y1 - y2) * (x3 - x4)) ||
+            (abs((x1 - x3) * (y2 - y4)) == abs((y1 - y3) * (x2 - x4))) ||
+            (abs((x1 - x4) * (y2 - y3)) == abs((y1 - y4) * (x2 - x3)))
+        ) 1 else 0
+    }
 }
