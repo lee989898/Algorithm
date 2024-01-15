59e766db10ad364dc163249b3f268137adf3de16
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-15 15:33:38 +0900

    [level Lv.0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120808. 분수의 덧셈/분수의 덧셈.kt
+++ 프로그래머스/Lv.0/120808. 분수의 덧셈/분수의 덧셈.kt
@@ -2,7 +2,7 @@ class Solution {
     fun solution(numer1: Int, denom1: Int, numer2: Int, denom2: Int): IntArray {
         val child = numer1 * denom2 + numer2 * denom1
         val parent = denom1 * denom2
-        val gcd = gcd(maxOf(child, parent), minOf(child, parent))
+        val gcd = gcd(child, parent)
 
         return intArrayOf(child / gcd, parent / gcd)
     }
