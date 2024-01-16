60a53fdece1ec57cec7c459a2b10aaf393c73d45
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-16 10:29:09 +0900

    [level Lv.0] Title: 정수를 나선형으로 배치하기, Time: 0.92 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.kt
+++ 프로그래머스/Lv.0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.kt
@@ -1,49 +1,46 @@
 class Solution {
     fun solution(n: Int): Array<IntArray> {
         val answer = Array(n) { IntArray(n) }
-
-        var i = 0
-        var j = 0
+        var (i, j) = 0 to 0
         var num = 1
-        var direction = "right"
+        var direction = DIRECTION.RIGHT
 
         while (num <= n * n) {
-
             answer[i][j] = num++
 
             when (direction) {
-                "right" -> {
+                DIRECTION.RIGHT -> {
                     if (j + 1 < n && answer[i][j + 1] == 0) {
                         j += 1
                     } else {
-                        direction = "down"
+                        direction = DIRECTION.DOWN
                         i += 1
                     }
                 }
 
-                "down" -> {
+                DIRECTION.DOWN -> {
                     if (i + 1 < n && answer[i + 1][j] == 0) {
                         i += 1
                     } else {
-                        direction = "left"
+                        direction = DIRECTION.LEFT
                         j -= 1
                     }
                 }
 
-                "left" -> {
+                DIRECTION.LEFT -> {
                     if (j - 1 >= 0 && answer[i][j - 1] == 0) {
                         j -= 1
                     } else {
-                        direction = "up"
+                        direction = DIRECTION.UP
                         i -= 1
                     }
                 }
 
-                else -> {
+                DIRECTION.UP -> {
                     if (i - 1 >= 0 && answer[i - 1][j] == 0) {
                         i -= 1
                     } else {
-                        direction = "right"
+                        direction = DIRECTION.RIGHT
                         j += 1
                     }
                 }
@@ -52,4 +49,8 @@ class Solution {
 
         return answer
     }
+}
+
+enum class DIRECTION {
+    UP, DOWN, LEFT, RIGHT
 }
