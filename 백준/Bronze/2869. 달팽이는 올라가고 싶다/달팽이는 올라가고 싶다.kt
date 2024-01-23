be8515e726e9e1ea47b2fc95ba09967dda58ed2d
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-23 20:39:16 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 140 ms, Memory: 20864 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.kt
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.kt
@@ -1,27 +1,10 @@
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-import kotlin.math.sqrt
-
 fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
-
-    var (a, b, v) = br.readLine().split(" ").map { it.toInt() }
+    var (a, b, v) = readln().split(" ").map { it.toInt() }
     v -= b
-    var count = 0
 
-    if (v % (a - b) != 0) {
-        count += v / (a - b)
-        count += 1
+    if (v % (a - b) == 0) {
+        print(v / (a - b))
     } else {
-        count += v / (a - b)
+        print(v / (a - b) + 1)
     }
-
-    bw.write("$count")
-
-    bw.flush()
-    bw.close()
-    br.close()
 }
