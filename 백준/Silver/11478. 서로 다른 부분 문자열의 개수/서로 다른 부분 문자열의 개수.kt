92bda5d5990de9de48dd48fbff7ddd56a19a19db
Lee Joonkyung <wnsrud1352@gmail.com>
2024-01-29 14:31:59 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 1100 ms, Memory: 560444 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.kt
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.kt
@@ -2,13 +2,9 @@ fun main() {
     val s = readln()
     val set = HashSet<String>()
 
-    for (i in s.indices) {
-        for (j in s.indices) {
-            val slice = s.slice(i..j)
-
-            if (slice.isNotBlank()) {
-                set.add(slice)
-            }
+    for (i in 0..s.length) {
+        for (j in i + 1..s.length) {
+            set.add(s.substring(i..<j))
         }
     }
 
