94288a6914ce93588af76a4b749cd403417a3e43
Lee Joonkyung <wnsrud1352@gmail.com>
2024-02-04 00:05:13 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 160 ms, Memory: 22800 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.kt
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.kt
@@ -1,51 +1,51 @@
-import kotlin.math.min
-
 fun main() {
     val (n, m) = readln().split(" ").map { it.toInt() }
     val chess = Array(n) { readln().toCharArray() }
     var result = Int.MAX_VALUE
 
-    fun countBoard(
+    fun countWBoard(
         x: Int,
         y: Int,
     ): Int {
-        var count1 = 0
-        var count2 = 0
+        var count = 0
 
         for (i in x..<x + 8) {
             for (j in y..<y + 8) {
-                if (i % 2 == 0 && j % 2 == 0 && chess[i][j] != 'W') {
-                    count1 += 1
-                } else if (i % 2 == 0 && j % 2 != 0 && chess[i][j] != 'B') {
-                    count1 += 1
-                } else if (i % 2 != 0 && j % 2 != 0 && chess[i][j] != 'W') {
-                    count1 += 1
-                } else if (i % 2 != 0 && j % 2 == 0 && chess[i][j] != 'B') {
-                    count1 += 1
+                when {
+                    i % 2 == 0 && j % 2 == 0 && chess[i][j] != 'W' -> count++
+                    i % 2 == 0 && j % 2 != 0 && chess[i][j] != 'B' -> count++
+                    i % 2 != 0 && j % 2 != 0 && chess[i][j] != 'W' -> count++
+                    i % 2 != 0 && j % 2 == 0 && chess[i][j] != 'B' -> count++
                 }
             }
         }
 
+        return count
+    }
+
+    fun countBBoard(
+        x: Int,
+        y: Int,
+    ): Int {
+        var count = 0
+
         for (i in x..<x + 8) {
             for (j in y..<y + 8) {
-                if (i % 2 == 0 && j % 2 == 0 && chess[i][j] != 'B') {
-                    count2 += 1
-                } else if (i % 2 == 0 && j % 2 != 0 && chess[i][j] != 'W') {
-                    count2 += 1
-                } else if (i % 2 != 0 && j % 2 != 0 && chess[i][j] != 'B') {
-                    count2 += 1
-                } else if (i % 2 != 0 && j % 2 == 0 && chess[i][j] != 'W') {
-                    count2 += 1
+                when {
+                    i % 2 == 0 && j % 2 == 0 && chess[i][j] != 'B' -> count++
+                    i % 2 == 0 && j % 2 != 0 && chess[i][j] != 'W' -> count++
+                    i % 2 != 0 && j % 2 != 0 && chess[i][j] != 'B' -> count++
+                    i % 2 != 0 && j % 2 == 0 && chess[i][j] != 'W' -> count++
                 }
             }
         }
-        
-        return min(count1, count2)
+
+        return count
     }
 
     for (i in 0..n - 8) {
         for (j in 0..m - 8) {
-            result = min(result, countBoard(i, j))
+            result = minOf(result, countWBoard(i, j), countBBoard(i, j))
         }
     }
 
